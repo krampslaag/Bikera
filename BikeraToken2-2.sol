2870b6fc330937f87c0ab361767ad23c3c2c566c
krampslaag <81129800+krampslaag@users.noreply.github.com>
2024-03-08 23:16:48 +0100

    Deployed contract on Ethereum

---
+++ BikeraToken2-2.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "Bikera Solidity Contract/openzeppelin-contracts-5.0.1/contracts/token/ERC20/ERC20.sol";
+import "Bikera Solidity Contract/openzeppelin-contracts-5.0.1/contracts/access/Ownable.sol";
+import "Bikera Solidity Contract/openzeppelin-contracts-5.0.1/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Bikera is ERC20, Ownable, ERC20Burnable {
+    // Constructor
+    uint256 private immutable _cap;
+    error ExceededCap(uint256 cap); // Shortened error message for gas optimization
+    error InvalidCap(); // Simplified error
+    constructor(address initialOwner, uint256 cap_) 
+    ERC20("Bikera", "MERA") 
+    
+    Ownable(initialOwner) {
+        if (cap_ == 0) {
+            revert InvalidCap();
+        }
+        _cap = cap_;
+    }
+    
+    function cap() public view virtual returns (uint256) {
+        return _cap;
+    }
+
+
+    function mint(address to, uint256 value) public onlyOwner {
+        require(totalSupply() + value <= cap(), "MERA cap exceeded, no infinte coins please");
+        _mint(to, value);
+    }
+}
