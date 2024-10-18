30eacfba53f62165ec810019e8ae06371ccd7255
krampslaag <81129800+krampslaag@users.noreply.github.com>
2024-10-18 02:39:56 +0200

    Update MKRWAN_PAYLOAD_CUSTOM_BIKERA.ino

--- Arduino firmware prototype/MKRWAN_PAYLOAD_CUSTOM_BIKERA.ino
+++ Arduino firmware prototype/MKRWAN_PAYLOAD_CUSTOM_BIKERA.ino
@@ -24,30 +24,31 @@ BikeraConnectionSaveData data;
 String nwkSKey;
 String appSKey;
 
-int PrivateKeyGenChoice = 0;
+byte PrivateKeyGenChoice = 0;
 String PrivateKeyLockOwner = "";
-int PrivateKeyGenChoiceVerify = 0;
+byte PrivateKeyGenChoiceVerify = 0;
 
-int StorePrivateKey = 0;
+byte StorePrivateKey = 0;
 byte PrivateKeyLockOwnerBuffer[32];
-int slot = 0;
+byte slot = 0;
 byte publicKey[64];
-int publicKeyLength;
+byte publicKeyLength;
 
 static constexpr uint32_t EXECUTION_PERIOD = 50;    // [msec.]static WM1110_Geolocation& wm1110_geolocation = WM1110_Geolocation::getInstance();
 
 FlashStorage(BikeraNetworkAppKeyAppEuiSaved, bool);
 FlashStorage(BikeraNetworkAppKey, String);
 FlashStorage(BikeraNetworkAppEui, String);
-FlashStorage(CurrentFreeSlot, int);
-FlashStorage(SavedPrivateKeySlot, int);
+FlashStorage(CurrentFreeSlot, byte);
+FlashStorage(SavedPrivateKeySlot, byte);
+FlashStorage(ReadyForConfiguration,byte); 
 
   
 struct BikeraSaveData {
   String appEui;
   String appKey;
   String devAddr;
-  bool OTAAConnectionEstablished;
+  byte OTAAConnectionEstablished;
 }
 
 struct BikeraLora { // Define the structure of a LoRaWAN packet
@@ -151,12 +152,13 @@ if (!ECCX08.locked()) { //at startup the
     while (!Serial.available());
     PrivateKeyGenChoice = Serial.readStringUntil('\n').toInt();
     if (PrivateKeyGenChoice == 1) {
-        Serial.println("Enter your private key, make sure it is correct because this device is write only once protected!")
+        Serial.println("Enter your private key, make sure it is correct because this device is write only once protected! ")
+        Serial.println("the Private key is inputted as 2 hexadecimal values per byte, totalling to 64 hexadecimal numbers for 32Byte or 256bit encryption.")
         Serial.readBytesuntil('\n', PrivateKeyLockOwnerBuffer, 32); 
         Serial.println("this is the private key you entered");
         for (int i = 0; i < 32; i++) {
-            Serial.print(PrivateKeyLockOwnerBuffer[i] >> 4, HEX);
-            Serial.print(PrivateKeyLockOwnerBuffer[i] & 0x0f, HEX);
+            Serial.print(PrivateKeyLockOwnerBuffer[i] >> 4, HEX); // print last 4 bit as hexadecimal number
+            Serial.print(PrivateKeyLockOwnerBuffer[i] & 0x0f, HEX); // print first 4 bit in hexadecimal format
             }
         Serial.println("Do you wish to store this permanently in the lock? (1)YES or (2)NO.");
         StorePrivateKey = Serial.readStringUntil('\n').toInt();
@@ -165,37 +167,53 @@ if (!ECCX08.locked()) { //at startup the
             while (1) {}
             }
         if (StorePrivateKey == 1) {
-            for (int i = 0; i < 32; i++) {
-                PrivateKeyLockOwnerBuffer[i].writeSlot(); //private key should should be able to be loaded in slot 1 or 2, 
-                }
-            sizeof(publicKey) = publicKeyLength;
+            ECCX08.writeSlot(slot, const PrivateKeyLockOwnerBuffer[], 32); //private key should should be able to be loaded in slot 1 or 2, 
+            }
+            ECCX08.generatePublicKey(Slot, publicKey[] );
+            sizeof(publicKey) = publicKeyLength; //if you're using a private key used in Bitcoin or Ethereum; this will not generate the same Public key
             Serial.println("this is your corresponding public key");
             for (int i = 0; i < publicKeyLength; i++) {  /* data is written and stored on the eccX08 chip as an array of bytes. 
-                                                  SHA256 ECDSA on elliptic curve p-256 is available, this isn't used in any blockchain today but this will probably be the 
-                                                  one we will use on th local sidechains.
+                                                  SHA256 ECDSA on elliptic curve p-256 is available, this isn't used in many blockchain today but this will probably be the 
+                                                  one we will use on th local sidechains, due to the memory restraints and power consumption by running A BIP66 
+                                                  ipmlemantation for the SAMD21 chip, itterations and field testing will provide details on power consumption
+                                                  and the purpose of having/needing a crypto chip onboard.. ( NIST p-256 curve, is used with NEO and ontology)
                                                   needed for compatibility of accesible hardware 
-                                                  
                                                   ECDH private key locked in device, should be used only for verifying this device/chip and communications over LoRa
                                                   */
                 Serial.print(input[i] >> 4, HEX);
                 Serial.print(input[i] & 0x0f, HEX);
                 }
-            Serial.println();
-            }
-        PrivateKeyLockOwnerBuffer = {}; // write the data holder of the private key empty so that no trace of the private key in flash memory of not secure part is deleted
-        ECCX08.generatePublicKey(Slot, publicKey);
-        
-        Serial.println("does this public key match the private key pair? (1)'YES' OR (2)'NO'.")
-        while (!Serial.available());
-        PrivateKeyGenChoiceVerify = Serial.readStringUntil('\n').toInt(); 
-    }  
-    if (PrivateKeyGenChoice == 2){
-        
-      
-        }
-}
+             Serial.println();
+             PrivateKeyLockOwnerBuffer = {}; // write the data holder of the private key empty so that no trace of the private key in flash memory 
+             Serial.println("does this public key match the private key pair? (1)'YES' OR (2)'NO'.")
+             while (!Serial.available());
+             PrivateKeyGenChoiceVerify = Serial.readStringUntil('\n').toInt(); 
+             if ( PrivateKeyGenChoiceVerify == 1 ) {
+                Serial.println("Succes, the device will now configure and lock this data");
+                SavedPrivateKeySlot = 1;
+                }
+             if ( PrivateKeyGenChoiceVerify == 2 ){
+               Serial.println("Try another curve then secp256k1, Last chance to pull the power and restart")
+               Serial.println("Go YOLO and press ENTER to continue")
+               Serial.readStringUntil('\n');
+               SavedPrivateKeySlot = 1;
+               }
+      }         
 
-if ( PrivateKeyGenChoiceVerify == 1 || PrivateKeyGenChipOk == 1 ){
+      if (PrivateKeyGenChoice == 2){
+          ECCX08.generatePrivateKey(Slot, publicKey[] );
+          ECCX08.generatePublicKey(Slot, publicKey[] );
+          sizeof(publicKey) = publicKeyLength; //if you're using a private key used in Bitcoin or Ethereum; this will not generate the same Public key
+          Serial.println("this is your corresponding public key");
+          for (int i = 0; i < publicKeyLength; i++) {
+                Serial.print(input[i] >> 4, HEX);
+                Serial.print(input[i] & 0x0f, HEX);
+                }  
+          }
+      
+    }
+
+if (  SavedPrivateKeySlot == 1 || PrivateKeyGenChipOk == 1 ){
   
 }
 }
