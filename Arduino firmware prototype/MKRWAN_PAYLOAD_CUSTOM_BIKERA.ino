785dff824d214575705b0dce36ef0bfcfb05e9e1
krampslaag <81129800+krampslaag@users.noreply.github.com>
2024-10-21 05:09:45 +0200

    Update MKRWAN_PAYLOAD_CUSTOM_BIKERA.ino
    Debuugggiiiinnnngggg

--- Arduino firmware prototype/MKRWAN_PAYLOAD_CUSTOM_BIKERA.ino
+++ Arduino firmware prototype/MKRWAN_PAYLOAD_CUSTOM_BIKERA.ino
@@ -1,29 +1,26 @@
 #include <SPI.h>
-#include <Uart.h>
+#include <Wire.h>
 #include <Arduino.h>
 #include <MKRWAN.h>
-#include <WM1110_Geolocation.hpp>
-#include <LbmWm1110.hpp>
-#include <Lbmx.hpp>
-#include <Tracker_Peripheral.hpp>
-#include <Lbm_Modem_Common.hpp>
 #include <ArduinoECCX08.h>
 #include <SD.h>
 #include <FlashStorage.h>
-#include <Entropy.h>
+#include <AlmostRandom.h>
+#include <ECCX08_DEFAULT_BIKERA_CONFIG.h>
 
 #define PIN_SPI_CS 4
+#define MAX_PREAMBLE_SIZE 16
+#define MAX_PAYLOAD_SIZE 256
 
 File BikeraLog;
 String Err = "This is the last location before power OFF";
 byte SavedLastLocationBufferByteArray[] = {};
 
-BikeraLoRaModem modem;
-BikeraLoRaPacket packet;
-BikeraConnectionSaveData data;
-
 String nwkSKey;
 String appSKey;
+byte OTAAConnectionEstablished;
+String VerifyAppKeyAppEuiCorrect;
+bool ConfigurationChipOk = 0;
 
 byte PrivateKeyGenChoice = 0;
 String PrivateKeyLockOwner = "";
@@ -33,7 +30,7 @@ byte SavedPrivateKeyToSlot = 0;
 int StorePrivateKey = 0;
 byte ConfigurationAlreadyLoaded = 0;
 byte StoreConfiguration = 0;
-byte PrivateKeyLockOwnerBuffer[];
+byte PrivateKeyLockOwnerBuffer[32];
 byte slot = 0;
 byte publicKey[64];
 byte publicKeyLength;
@@ -45,28 +42,32 @@ FlashStorage(BikeraNetworkAppKey, String);
 FlashStorage(BikeraNetworkAppEui, String);
 FlashStorage(CurrentFreeSlot, byte);
 FlashStorage(SavedPrivateKeyToSlotFlash, bool);
-FlashStorage(ConfigurationAlreadyLoaded, bool);
+FlashStorage(ConfigurationAlreadyLoadedFlash, bool);
 FlashStorage(AppKeyAppEuiVerified, bool);
 
-  
-struct BikeraConnetionSaveData {
+struct LoRaSavedData {
   String appEui;
   String appKey;
   String devAddr;
-  byte OTAAConnectionEstablished;
+  bool verified;
 };
 
-struct BikeraLoraPacket { // Define the structure of a LoRaWAN packet
+struct LoRaBikeraPacket { // Define the structure of a LoRaWAN packet
   int connected;
-  uint8_t preamble[];
-  uint8_t payload[];
+  uint8_t* preamble;
+  uint8_t* payload; 
   size_t SendData = 0;
-  uint8_t lenght; //keeps track of the payload lenght
+  uint8_t length; //keeps track of the payload length
   int err;
+  uint8_t* payloadInput;
 };
 
+LoRaModem modem;
+LoRaSavedData data;
+LoRaBikeraPacket packet;
 
 void setup() {
+  
   Serial.begin(9600);
   while (!Serial);
   Serial.println("Welcome to Bikera");
@@ -85,32 +86,33 @@ void setup() {
   Serial.print("Your device EUI is: ");
   Serial.println(modem.deviceEUI());
 
-  BikeraNetworkAppKeyAppEuiSaved.read() = OTAAConnectionEstablished //Appeui and Appkey need to be entered, check weither it is already stored in flashdrive
+  OTAAConnectionEstablished = BikeraNetworkAppKeyAppEuiSaved.read(); //Appeui and Appkey need to be entered, check weither it is already stored in flashdrive
   if (!OTAAConnectionEstablished) {
     Serial.println("Enter The Bikera Network APP EUI and confirm by pressing Enter");
     while (!Serial.available());
-    appEui = Serial.readStringUntil('\n');
+    data.appEui = Serial.readStringUntil('\n');
 
     Serial.println("Enter The local Bikera sidechain APP KEY");
     while (!Serial.available());
-    appKey = Serial.readStringUntil('\n');
+    data.appKey = Serial.readStringUntil('\n');
 
-    appKey.trim();
-    appEui.trim();
+    data.appKey.trim();
+    data.appEui.trim();
     
-    BikeraNetworkAppKey.write(appKey);
-    BikeraNetworkAppEui.write(appEui);
+    BikeraNetworkAppKey.write(data.appKey);
+    BikeraNetworkAppEui.write(data.appEui);
     BikeraNetworkAppKeyAppEuiSaved.write(1);
     OTAAConnectionEstablished = 1;
   }
-  appKey = BikeraNetworkAppKey.read();
-  appEui = BikeraNetworkAppEui.read();
-  if (OTAAConnectionEstablished == 1 && !AppKeyAppEuiVerified ) {
+  data.appKey = BikeraNetworkAppKey.read();
+  data.appEui = BikeraNetworkAppEui.read();
+  data.verified = AppKeyAppEuiVerified.read();
+  if (OTAAConnectionEstablished == 1 && data.verified == 0 ) {
     Serial.println("this is the stored appEui:");
-    Serial.println(appEui);
+    Serial.println(data.appEui);
     Serial.println("this is the stored appKey:");
-    Serial.println(appKey);
-    Serial.println("Verify if this is correct and verify with (1)Yes or (2)No")
+    Serial.println(data.appKey);
+    Serial.println("Verify if this is correct and verify with (1)Yes or (2)No");
     VerifyAppKeyAppEuiCorrect = Serial.readStringUntil('\n').toInt();
     if (VerifyAppKeyAppEuiCorrect == 1) {
           AppKeyAppEuiVerified.write(1);
@@ -120,15 +122,17 @@ void setup() {
           Serial.println("appKey and appEui reset, unplug the power and restart the setup.");
     }
   }
-  if (OTAAConnectionEstablished == 1 && AppKeyAppEuiVerified == 1) {
-    connected = modem.joinOTAA(appEui, appKey); //this is confirmation of the network for connection activation
-    if (!connected) {
+  if (OTAAConnectionEstablished == 1 && data.verified == 1) {
+    packet.connected = modem.joinOTAA(data.appEui, data.appKey); //this is confirmation of the network for connection activation
+    if (!packet.connected) {
           Serial.println("Something went wrong; are you indoor? Move near a window and retry");
           while (1) {}
     }
     Serial.println("connecting to IoT network");
   }
-
+  packet.preamble = (uint8_t*)malloc(MAX_PREAMBLE_SIZE * sizeof(uint8_t)); // allocate memory for dynamic size array 
+  packet.payload = (uint8_t*)malloc(MAX_PAYLOAD_SIZE * sizeof(uint8_t));
+  packet.payloadInput = (uint8_t*)malloc(MAX_PAYLOAD_SIZE * sizeof(uint8_t));
   
   // Initialize ATEC608 or ATEC508 chip
   if (!ECCX08.begin()) {
@@ -140,11 +144,11 @@ void setup() {
     Serial.println("The ECC508/ECC608 is not locked! and you need to enter a private key for this BikeraLockDevice");
     Serial.println("Enter the number to choose (1) Input private key or (2) Generate private key");
     while (!Serial.available());
-    PrivateKeyGenChoice = Serial.readByteUntil('\n');
+    PrivateKeyGenChoice = Serial.read();
     if (PrivateKeyGenChoice == 1) {
-        Serial.println("Enter your private key, make sure it is correct because this device is write only once protected! ")
-        Serial.println("the Private key is inputted as 2 hexadecimal values per byte, totalling to 64 hexadecimal numbers for 32Byte or 256bit encryption.")
-        Serial.readBytesuntil('\n', PrivateKeyLockOwnerBuffer, 32); 
+        Serial.println("Enter your private key, make sure it is correct because this device is write only once protected! ");
+        Serial.println("the Private key is inputted as 2 hexadecimal values per byte, totalling to 64 hexadecimal numbers for 32Byte or 256bit encryption.");
+        Serial.readBytesUntil('\n', PrivateKeyLockOwnerBuffer, 32); 
         Serial.println("this is the private key you entered");
         for (int i = 0; i < 32; i++) {
             Serial.print(PrivateKeyLockOwnerBuffer[i] >> 4, HEX); // print last 4 bit as hexadecimal number
@@ -157,10 +161,10 @@ void setup() {
             while (1);
             }
         if (StorePrivateKey == 1) {
-            ECCX08.writeSlot(slot, const PrivateKeyLockOwnerBuffer[], 32); //private key should should be able to be loaded in slot 1 or 2, 
+            ECCX08.writeSlot(slot, PrivateKeyLockOwnerBuffer, 32); //private key should should be able to be loaded in slot 1 or 2, 
             }
-            ECCX08.generatePublicKey(Slot, publicKey[] );
-            sizeof(publicKey) = publicKeyLength; //if you're using a private key used in Bitcoin or Ethereum; this will not generate the same Public key
+            ECCX08.generatePublicKey(slot, publicKey);
+            publicKeyLength = sizeof(publicKey);  //if you're using a private key used in Bitcoin or Ethereum; this will not generate the same Public key
             Serial.println("this is your corresponding public key");
             for (int i = 0; i < publicKeyLength; i++) {  /* data is written and stored on the eccX08 chip as an array of bytes. 
                                                   SHA256 ECDSA on elliptic curve p-256 is available, this isn't used in many blockchain today but this will probably be the 
@@ -170,12 +174,12 @@ void setup() {
                                                   needed for compatibility of accesible hardware 
                                                   ECDH private key locked in device, should be used only for verifying this device/chip and communications over LoRa
                                                   */
-                Serial.print(input[i] >> 4, HEX);
-                Serial.print(input[i] & 0x0f, HEX);
+                Serial.print(publicKey[i] >> 4, HEX);
+                Serial.print(publicKey[i] & 0x0f, HEX);
                 }
              Serial.println();
-             PrivateKeyLockOwnerBuffer = {}; // write the data holder of the private key empty so that no trace of the private key in flash memory 
-             Serial.println("does this public key match the private key pair? (1)'YES' OR (2)'NO'.")
+             memset(PrivateKeyLockOwnerBuffer, 0, sizeof(PrivateKeyLockOwnerBuffer));  // write the data holder of the private key empty so that no trace of the private key in flash memory 
+             Serial.println("does this public key match the private key pair? (1)'YES' OR (2)'NO'.");
              while (!Serial.available());
              PrivateKeyGenChoiceVerify = Serial.readStringUntil('\n').toInt(); 
              if ( PrivateKeyGenChoiceVerify == 1 ) {
@@ -194,13 +198,13 @@ void setup() {
       }         
 
       if (PrivateKeyGenChoice == 2){
-          ECCX08.generatePrivateKey(Slot, publicKey[] );
-          ECCX08.generatePublicKey(Slot, publicKey[] );
-          sizeof(publicKey) = publicKeyLength; //if you're using a private key used in Bitcoin or Ethereum; this will not generate the same Public key
+          ECCX08.generatePrivateKey(slot, publicKey);
+          ECCX08.generatePublicKey(slot, publicKey);
+          publicKeyLength = sizeof(publicKey); //if you're using a private key used in Bitcoin or Ethereum; this will not generate the same Public key
           Serial.println("this is your corresponding public key");
           for (int i = 0; i < publicKeyLength; i++) {
-                Serial.print(input[i] >> 4, HEX);
-                Serial.print(input[i] & 0x0f, HEX);
+                Serial.print(publicKey[i] >> 4, HEX);
+                Serial.print(publicKey[i] & 0x0f, HEX);
                 } 
           SavedPrivateKeyToSlotFlash.write(1);
           StoreConfiguration = 1;
@@ -213,12 +217,12 @@ void setup() {
           ConfigurationAlreadyLoadedFlash.write(1);
   }
   if (ECCX08.readConfiguration(ECCX08_DEFAULT_BIKERA_CONFIG)) {
-          Serial.println("EECX08 configuration loaded correctly.")
+          Serial.println("EECX08 configuration loaded correctly.");
           ConfigurationChipOk = 1;
   }
   if (!ECCX08.readConfiguration(ECCX08_DEFAULT_BIKERA_CONFIG)) {
-          Serial.println("Configuration file loading failed or is not the same as first entry, check if file is corrupted!")
-          Serial.println("Press enter to restart the device to load the current updated or altered configuration file or unplug the power to retry."
+          Serial.println("Configuration file loading failed or is not the same as first entry, check if file is corrupted!");
+          Serial.println("Press enter to restart the device to load the current updated or altered configuration file or unplug the power to retry.");
           Serial.readStringUntil('\n');
           ConfigurationAlreadyLoadedFlash.write(0);
   }
@@ -237,35 +241,35 @@ void setup() {
 }
 
 // Function to create a LoRaWAN packet
-void createLoRaWANPacket(BikeraLora &packet, uint8_t *payload, uint8_t payloadSize) {
+void createLoRaWANPacket(uint8_t *payLoad, uint8_t payloadSize) {
   int EncodeMessage = 0; 
   byte signature[64];
-  uint8_t payloadSize;
+  byte RandomBytenumber;
   // Fill the preamble
-  packet.preamble[] = [33, 48, 68]; // Example preamble value, should this have random generated numbers? check for further connection..
-
+  uint8_t preamble[] = {33, 48, 68}; // Example preamble value, should this have random generated numbers? check for further connection..
+  memcpy(packet.preamble, preamble, sizeof(preamble));
+  
   if (Serial.available() > 0) { // testing if input is comming through the network with right output 
     // Read the payload from serial input
-    Serial.readBytesuntil('\n', PayloadInput);//  manual input coordinates of wio or gps module
-    packet.payloadSize = PayloadInput.length();
-    Serial.println("Encode message with private key on-board lock (1)Yes or (2)No")
+    Serial.readBytesUntil('\n', packet.payloadInput, sizeof(packet.payloadInput));//  manual input coordinates of wio or gps module
+    Serial.println("Encode message with private key on-board lock (1)Yes or (2)No");
     EncodeMessage = Serial.read();
     if (EncodeMessage == 1) {
-      ECCX08.ecSign(slot, PayloadInput, signature);
+      ECCX08.ecSign(slot, packet.payloadInput, signature);
       for(int i=0; i < 64; i++) {
-        packet.payload[i] = signature[i];
-        packet.payloadSize = 64;
+        payLoad[i] = signature[i];
+        payloadSize = 64;
       }
     if (EncodeMessage == 2) {
       for(int i=0; i < payloadSize; i++) {
-      packet.payload[i] = PayloadInput[i]; 
+      packet.payload[i] = packet.payloadInput[i]; 
   }
   if (!Serial.available()) { // lock is not connected to a computer or serial input device
     for(int i=0; i < 64; i++) { /*for testing purpose we will let the MCU generated random data, in the future this data should be encrypted for the bikera blockchain sidechain
                                 to decypher and store GPS coordinates on the ledger if needed, gps module inbound ETA december added in code.
                                 64 bytes of data, this is a SHA-256 digest. 
                                 */   
-      uint8_t RandomBytenumber = Entropy.randomByte();
+      RandomBytenumber = TrueRandom.randomByte()
       packet.payload[i] = RandomBytenumber;
       payloadSize = 64;
     }
@@ -296,4 +300,6 @@ void loop() {
     Serial.println("(you may send a limited amount of messages per minute, depending on the signal strength");
     Serial.println("it may vary from 1 message every couple of seconds to 1 message every minute)");
   }
+  free(data.preamble);
+  free(data.payload);
 }
